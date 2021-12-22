7756cf42dd2e70a386387a69d2a0da8387074b31
aapartyka <76936962+aapartyka@users.noreply.github.com>
2021-12-22 22:03:30 +0100

    add Pokemon list
    added Pokemon list with sample Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
-alert('Hello World');
-let favoriteFood = 'grilled octopus';
-document.write(favoriteFood);
+let pokemonList = [];
+
+
+/*
+Array which stores the pokemon objects and it's attributes.
+Attributes: Number in Pokedex, name, type, category, height, weight, gender, abilities 
+*/
+pokemonList = [
+    {
+        pokedexNumber: 4,
+        name: 'Charmander',
+        type: ['Fire'],
+        height: 2.0,
+        weight: 18.7,
+    },
+
+    {
+        pokedexNumber: 5,
+        name: 'Charmeleon',
+        type: ['Fire'],
+        height: 3.07,
+        weight: 41.9,
+    },
+
+    {
+        pokedexNumber: 6,
+        name: 'Charizard',
+        type: ['Fire', 'Flying'],
+        height: 5.07,
+        weight: 199.5,
+    },
+
+    {
+        pokedexNumber: 65,
+        name: 'Alakazam  ',
+        type: ['Psychic'],
+        height: 4.11,
+        weight: 105.08,
+    }
+]
