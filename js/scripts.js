d082796d63f0d1a93db25f81e83b01e53bf05722
aapartyka <76936962+aapartyka@users.noreply.github.com>
2021-12-23 14:08:35 +0100

    implementation of a for loop for displaying Pokemon
    implementation of a for loop which displays every Pokemon in pokemonList[].

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonList = [];
 
 /*
 Array which stores the pokemon objects and it's attributes.
-Attributes: Number in Pokedex, name, type, category, height, weight, gender, abilities 
+Attributes: Number in Pokedex, name, type, category, height, weight, gender, abilities
 */
 pokemonList = [
     {
@@ -13,7 +13,6 @@ pokemonList = [
         height: 2.0,
         weight: 18.7,
     },
-
     {
         pokedexNumber: 5,
         name: 'Charmeleon',
@@ -21,7 +20,6 @@ pokemonList = [
         height: 3.07,
         weight: 41.9,
     },
-
     {
         pokedexNumber: 6,
         name: 'Charizard',
@@ -29,12 +27,30 @@ pokemonList = [
         height: 5.07,
         weight: 199.5,
     },
-
     {
         pokedexNumber: 65,
-        name: 'Alakazam  ',
+        name: 'Alakazam',
         type: ['Psychic'],
         height: 4.11,
         weight: 105.08,
+    },
+    {
+        pokedexNumber: 10,
+        name: 'Caterpie',
+        type: ['Bug'],
+        height: 1.00,
+        weight: 105.08,
     }
 ]
+// for loop that displays attributes for every pokemon in pokemonList[n]
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<br>' + pokemonList[i].name + " " + '(height: ' + pokemonList[i].height + ')');
+    //All pokemon greater than 5 feet
+    if (pokemonList[i].height >= 5.0) {
+        document.write(' - Wow, that\'s really big');
+    }
+    // All pokemon smaller than 1.1 feet
+    else if (pokemonList[i].height < 1.1){
+        document.write(' - Cute, that\'s a really small');
+    }
+}
